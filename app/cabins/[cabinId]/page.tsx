61da11b95f429b7ee6d7d1d12ec5b1abc9285ded
milan <milanzivanov@users.noreply.github.com>
2025-01-14 13:16:23 +0100

    test 1

--- app/cabins/[cabinId]/page.tsx
+++ app/cabins/[cabinId]/page.tsx
@@ -1,21 +1,78 @@
-import { getCabin, getCabins } from "@/app/_lib/data-service";
-import Reservation from "@/app/_components/Reservation";
-import { Suspense } from "react";
-import Spinner from "@/app/_components/Spinner";
-import Cabin from "@/app/_components/Cabin";
-import { Cabin as CabinType } from "@/app/types";
+// import { getCabin, getCabins } from "@/app/_lib/data-service";
+// import Reservation from "@/app/_components/Reservation";
+// import { Suspense } from "react";
+// import Spinner from "@/app/_components/Spinner";
+// import Cabin from "@/app/_components/Cabin";
+// import { Cabin as CabinType } from "@/app/types";
+
+// type Props = {
+//   params: Promise<{ cabinId: string }>;
+// };
+
+// export async function generateMetadata({ params }: Props) {
+//   const cabin = await getCabin((await params).cabinId);
+
+//   return {
+//     title: `Cabin ${cabin?.name}`
+//   };
+// }
+
+// export async function generateStaticParams() {
+//   const cabins = await getCabins();
+
+//   return cabins.map((cabin) => ({
+//     params: { cabinId: cabin.id.toString() }
+//   }));
+// }
+
+// export default async function Page({ params }: Props) {
+//   const { cabinId } = await params;
+
+//   const cabin = await getCabin(cabinId);
+
+//   const cabinData: CabinType = {
+//     id: cabin.id,
+//     created_at: cabin.created_at,
+//     name: cabin.name!,
+//     discount: cabin.discount!,
+//     image: cabin.image!,
+//     maxCapacity: cabin.maxCapacity!,
+//     regularPrice: cabin.regularPrice!,
+//     description: cabin.description!
+//   };
+
+//   return (
+//     <div className="max-w-6xl mx-auto mt-8">
+//       <div className="max-w-6xl mx-auto mt-8">
+//         <Cabin cabin={cabinData} />
+
+//         <div>
+//           <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
+//             Reserve {cabinData.name} today. Pay on arrival.
+//           </h2>
+
+//           <Suspense fallback={<Spinner />}>
+//             <Reservation cabin={cabinData} />
+//           </Suspense>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+import { getCabins } from "@/app/_lib/data-service";
 
 type Props = {
   params: Promise<{ cabinId: string }>;
 };
 
-export async function generateMetadata({ params }: Props) {
-  const cabin = await getCabin((await params).cabinId);
+// export async function generateMetadata({ params }: Props) {
+//   const cabin = await getCabin((await params).cabinId);
 
-  return {
-    title: `Cabin ${cabin?.name}`
-  };
-}
+//   return {
+//     title: `Cabin ${cabin?.name}`
+//   };
+// }
 
 export async function generateStaticParams() {
   const cabins = await getCabins();
@@ -28,34 +85,5 @@ export async function generateStaticParams() {
 export default async function Page({ params }: Props) {
   const { cabinId } = await params;
 
-  const cabin = await getCabin(cabinId);
-
-  const cabinData: CabinType = {
-    id: cabin.id,
-    created_at: cabin.created_at,
-    name: cabin.name!,
-    discount: cabin.discount!,
-    image: cabin.image!,
-    maxCapacity: cabin.maxCapacity!,
-    regularPrice: cabin.regularPrice!,
-    description: cabin.description!
-  };
-
-  return (
-    <div className="max-w-6xl mx-auto mt-8">
-      <div className="max-w-6xl mx-auto mt-8">
-        <Cabin cabin={cabinData} />
-
-        <div>
-          <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
-            Reserve {cabinData.name} today. Pay on arrival.
-          </h2>
-
-          <Suspense fallback={<Spinner />}>
-            <Reservation cabin={cabinData} />
-          </Suspense>
-        </div>
-      </div>
-    </div>
-  );
+  return <div>hello world {cabinId}</div>;
 }
